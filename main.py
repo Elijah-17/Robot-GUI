4eaa2ad5450d3c45be38f927902f5172de72433a
Elijah-17 <105989898+Elijah-17@users.noreply.github.com>
2024-06-01 17:56:58 +0000

    setup

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import tk
 import customtkinter
-#test GUI
+#system appearance and config
+customtkinter.set_appearance_mode('System')
+customtkinter.set_default_color_theme('blue')
+
+app = customtkinter.CTk()
+app.geometry('1920x1080')
+app.title('button')
 
 #idk what im doing
 

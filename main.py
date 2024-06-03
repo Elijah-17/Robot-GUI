d146c70995273c70cc887f17799763f728171d47
Elijah <elijahreichart@gmail.com>
2024-06-03 16:26:50 -0400

    made button work and displayed text that robot is connected

--- main.py
+++ main.py
@@ -10,24 +10,24 @@ customtkinter.set_default_color_theme('blue')
 # Create the main window
 app = customtkinter.CTk()
 app.title('Robot GUI')
-app.geometry("1920x1080")
+app.geometry("920x1080")
 
 def activate():
     print('i was called')
-    text_frame = customtkinter.CTkFrame(app)
-    text_frame.pack(pady=20)
-    T = tk.Text(text_frame, height=2, width=30)
-    T.pack()
-    T.insert(tk.END, "Just a text Widget\nin two lines\n")
-        #disable joint lock, enable all axis, home robot, reset robot joint values.
+    ActivateButton.pack_forget()  # Hide the button
 
-        #add text that says 'robot connected'
-
-    
+    grey_box = customtkinter.CTkFrame(app, width=150, height=30, fg_color="darkgrey")
+    grey_box.place(x=100, y=100)
+    text_label = customtkinter.CTkLabel(app, text="Robot Connected", text_color="green", bg_color="darkgrey")
+    text_label.place(x=125, y=100)
+   
+        #disable joint lock, enable all axis, home robot, reset robot joint values. 
 
 # Create a button
-ActivateButton = customtkinter.CTkButton(app, text="Activate", command=activate) #command = ActivateButton.destroy
+ActivateButton = customtkinter.CTkButton(app, text="Activate", command=activate)
 ActivateButton.place(x = 100, y = 100)
+
+
 
 # Run the application
 app.mainloop()

7f1fc2e021407f37661ea4c48cf2cc4f2ef185b6
Elijah <elijahreichart@gmail.com>
2024-06-03 17:58:18 -0400

    added percent slider to all box sliders

--- main.py
+++ main.py
@@ -54,26 +54,27 @@ def update_slider_label(slider, label):
     label.configure(text=f"{value}%")
  #makes the sliders and titles, also displays robot rotation degrees for each joint
 def sliders():
-    box1_name = customtkinter.CTkLabel(app, text="Joint 1", bg_color='grey',font=("Arial", 10))
-    box1_name.place(x=600, y=10)
-    box1 = customtkinter.CTkSlider(app, from_=0, to=200)
-    box1.place(x=600, y=30)
-    box1.pack
+    # Slider 1
+    box1_name = customtkinter.CTkLabel(app, text="Joint 1", bg_color='grey', font=("Arial", 10))
+    box1_name.place(x=600, y=80)
+    box1 = customtkinter.CTkSlider(app, from_=0, to=100, command=lambda value: update_slider_label(box1, box1_percent))
+    box1.place(x=600, y=100)
+    box1_percent = customtkinter.CTkLabel(app, text="0.0%", bg_color='grey')
+    box1_percent.place(x=600, y=60)
 
-    box2 = customtkinter.CTkSlider(app, from_=0, to=200)
+    box2_name = customtkinter.CTkLabel(app, text="Joint 2", bg_color='grey', font=("Arial", 10))
+    box2_name.place(x=600, y=180)
+    box2 = customtkinter.CTkSlider(app, from_=0, to=100, command=lambda value: update_slider_label(box2, box2_percent))
     box2.place(x=600, y=200)
-    box2.pack
-    box3 = customtkinter.CTkSlider(app, from_=0, to=200)
+    box2_percent = customtkinter.CTkLabel(app, text="0.0%", bg_color='grey')
+    box2_percent.place(x=600, y=160)
 
-
-    # Slider 3
     box3_name = customtkinter.CTkLabel(app, text="Joint 3", bg_color='grey', font=("Arial", 10))
     box3_name.place(x=600, y=280)
     box3 = customtkinter.CTkSlider(app, from_=0, to=100, command=lambda value: update_slider_label(box3, box3_percent))
     box3.place(x=600, y=300)
-    box3_percent = customtkinter.CTkLabel(app, text="0%", bg_color='grey')
+    box3_percent = customtkinter.CTkLabel(app, text="0.0%", bg_color='grey')
     box3_percent.place(x=600, y=260)
-    app.update()
 
 
 

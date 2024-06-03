0ff218649e0472673aa2fbbc287e5ef458dfa4f5
Elijah <elijahreichart@gmail.com>
2024-06-03 15:59:26 -0400

    fixed some stuff

--- main.py
+++ main.py
@@ -13,9 +13,12 @@ app.title('Robot GUI')
 app.geometry("1920x1080")
 
 def activate():
-   T = tk.Text(app, height=2, width=30)
-   T.pack()
-   T.insert(tk.END, "Just a text Widget\nin two lines\n")
+    print('i was called')
+    text_frame = customtkinter.CTkFrame(app)
+    text_frame.pack(pady=20)
+    T = tk.Text(text_frame, height=2, width=30)
+    T.pack()
+    T.insert(tk.END, "Just a text Widget\nin two lines\n")
         #disable joint lock, enable all axis, home robot, reset robot joint values.
 
         #add text that says 'robot connected'
@@ -23,7 +26,7 @@ def activate():
     
 
 # Create a button
-ActivateButton = customtkinter.CTkButton(app, text="Activate", command = activate) #command = ActivateButton.destroy
+ActivateButton = customtkinter.CTkButton(app, text="Activate", command=activate) #command = ActivateButton.destroy
 ActivateButton.place(x = 100, y = 100)
 
 # Run the application
